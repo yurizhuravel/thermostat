8084b631c057382fceb3c7b26174a819e6aed6a3
William Osborne <wosborne@live.co.uk>
2016-08-03 17:41:45 +0100

    add power save mode toggle, reset button and display colour

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -5,21 +5,20 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   });
 
-    it('has a default temperature of 20 degrees', function() {
-      expect(thermostat.defaultTemperature).toEqual(20);
+    it('has a default temperature', function() {
+      expect(thermostat.temperature).toEqual(thermostat.DEFAULT_TEMPERATURE);
     });
 
     describe("Thermostat's temperature", function() {
 
       it('can be increased by pushing the Up button', function() {
         thermostat.pushUpButton();
-        thermostat.pushUpButton();
-        expect(thermostat.temperature).toEqual(22);
+        expect(thermostat.temperature).toEqual(thermostat.DEFAULT_TEMPERATURE + 1);
       });
 
       it('can be decreased by pushing the Down button', function() {
         thermostat.pushDownButton();
-        expect(thermostat.temperature).toEqual(19);
+        expect(thermostat.temperature).toEqual(thermostat.DEFAULT_TEMPERATURE - 1);
       });
 
       it('can not go lower than 10 degrees', function() {
@@ -27,12 +26,52 @@ describe('Thermostat', function() {
         expect(function(){ thermostat.pushDownButton(); }).toThrowError('Can not set the temperature below 10 degrees');
       });
 
-      it('can not go higher than 25 degrees in default power save mode', function() {
+      it('can be reset to default', function() {
+        thermostat.temperature = 28;
+        thermostat.resetTemperature();
+        expect(thermostat.temperature).toEqual(20);
+      })
+
+      describe('Power save mode', function() {
+
+      it('when ON, temperature can not exceed 25 degrees', function() {
         thermostat.temperature = 25;
         expect(function(){ thermostat.pushUpButton(); }).toThrowError('Can not set the temperature above 25 degrees in power save mode');
       });
 
+      it('when OFF, temperature can not exceed 32 degrees', function() {
+        thermostat.powerSaveMode = false;
+        thermostat.temperature = 32;
+        expect(function(){ thermostat.pushUpButton(); }).toThrowError('Can not set the temperature above 32');
+      });
+
+      it('can be switched OFF and ON', function() {
+        thermostat.togglePowerSave();
+        expect(thermostat.powerSaveMode).toEqual(false);
+        thermostat.togglePowerSave();
+        expect(thermostat.powerSaveMode).toEqual(true);
+      });
+
     });
 
+    describe('Thermostat display', function() {
+
+      it('is GREEN when temperature is below 18', function() {
+        thermostat.temperature = 17;
+        expect(thermostat.displayColour()).toEqual('green');
+      });
+
+      it('is YELLOW when temperature is between 18 and 24', function() {
+        thermostat.temperature = 22;
+        expect(thermostat.displayColour()).toEqual('yellow');
+      });
+
+      it('is RED when temperature is above 24', function() {
+        thermostat.temperature = 27;
+        expect(thermostat.displayColour()).toEqual('red');
+      });
+    });
+
+  });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,12 +1,15 @@
 function Thermostat() {
-  this.defaultTemperature = 20;
-  this.temperature = this.defaultTemperature;
+  this.DEFAULT_TEMPERATURE = 20;
+  this.temperature = this.DEFAULT_TEMPERATURE;
   this.powerSaveMode = true;
 }
 
 Thermostat.prototype.pushUpButton = function () {
   if(this.temperature === 25 && this.powerSaveMode) {
     throw new Error ('Can not set the temperature above 25 degrees in power save mode');
+  }
+  if(this.temperature === 32 && !this.powerSaveMode) {
+    throw new Error ('Can not set the temperature above 32');
   }
   this.temperature += 1;
 };
@@ -16,4 +19,22 @@ Thermostat.prototype.pushDownButton = function () {
     throw new Error ('Can not set the temperature below 10 degrees');
   }
   this.temperature -= 1;
+};
+
+Thermostat.prototype.togglePowerSave = function () {
+  this.powerSaveMode ? this.powerSaveMode = false : this.powerSaveMode = true;
+};
+
+Thermostat.prototype.resetTemperature = function () {
+  this.temperature = this.DEFAULT_TEMPERATURE;
+};
+
+Thermostat.prototype.displayColour = function () {
+  if(this.temperature < 18) {
+    return 'green';
+  }
+  if(this.temperature > 24) {
+    return 'red';
+  }
+  return 'yellow';
 };
