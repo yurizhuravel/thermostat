69f3c74756096fb4505ecf217281c4ef47f4b665
yurizhuravel <zhuravel@gmail.com>
2016-08-03 16:05:08 +0100

    Adds temperature limits

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -5,10 +5,34 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   });
 
-  describe('Has a default temperature', function() {
-
-    it('of 20 degrees', function() {
+    it('has a default temperature of 20 degrees', function() {
       expect(thermostat.defaultTemperature).toEqual(20);
     });
-  });
+
+    describe("Thermostat's temperature", function() {
+
+      it('can be increased by pushing the Up button', function() {
+        thermostat.pushUpButton();
+        thermostat.pushUpButton();
+        expect(thermostat.temperature).toEqual(22);
+      });
+
+      it('can be decreased by pushing the Down button', function() {
+        thermostat.pushDownButton();
+        expect(thermostat.temperature).toEqual(19);
+      });
+
+      it('can not go lower than 10 degrees', function() {
+        thermostat.temperature = 10;
+        expect(function(){ thermostat.pushDownButton(); }).toThrowError('Can not set the temperature below 10 degrees');
+      });
+
+      it('can not go higher than 25 degrees in default power save mode', function() {
+        thermostat.temperature = 25;
+        expect(function(){ thermostat.pushUpButton(); }).toThrowError('Can not set the temperature above 25 degrees in power save mode');
+      });
+
+    });
+
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,3 +1,19 @@
 function Thermostat() {
   this.defaultTemperature = 20;
+  this.temperature = this.defaultTemperature;
+  this.powerSaveMode = true;
 }
+
+Thermostat.prototype.pushUpButton = function () {
+  if(this.temperature === 25 && this.powerSaveMode) {
+    throw new Error ('Can not set the temperature above 25 degrees in power save mode');
+  }
+  this.temperature += 1;
+};
+
+Thermostat.prototype.pushDownButton = function () {
+  if(this.temperature === 10) {
+    throw new Error ('Can not set the temperature below 10 degrees');
+  }
+  this.temperature -= 1;
+};
